b9f8c9f94129d5d665f1acb54777514ca557711e
Fiyinfoluwa Akinsiku <fiyinakinsiku.b@gmail.com>
2020-08-29 02:48:24 +0100

    add tests

--- test/company.test.js
+++ test/company.test.js
@@ -15,16 +15,44 @@ describe("Server!", () => {
         done();
       });
   });
-//   it("adds a company", done => {
-//     chai
-//       .request(app)
-//       .post("/add")
-//       .send({ num1: 5, num2: 5 })
-//       .end((err, res) => {
-//         expect(res).to.have.status(200);
-//         expect(res.body.status).to.equals("success");
-//         expect(res.body.result).to.equals(10);
-//         done();
-//       });
-//   });
+
+  it("returns list of all companies", done => {
+    chai
+      .request(app)
+      .get("/api/companies")
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property('companies')
+        expect(res.body.companies).to.be.a('array');
+        done();
+      });
+  });
+
+  it("adds a company", done => {
+    let company = {
+      name: "Tech Base",
+      location: "Nigeria",
+      founder: "Fiyin Akinsiku",
+      year: 2020,
+      employees: 125,
+      website: "www.tech2base.com"
+  }
+    chai
+      .request(app)
+      .post("/api/add-company")
+      .send(company)
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.have.property('added');
+        expect(res.body.added).to.have.property('name');
+        expect(res.body.added).to.have.property('location');
+        expect(res.body.added).to.have.property('founder');
+        expect(res.body.added).to.have.property('year');
+        expect(res.body.added).to.have.property('employees');
+        expect(res.body.added).to.have.property('website');
+    done();
+      })
+  })
+
 });
