afbb4020573bc2e2addc831c24ee29e5adbef1f5
Fiyin-Anne <fiyinakinsiku.b@gmail.com>
2021-08-18 15:11:19 +0100

    change db server

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config()
 
 const app = express();
 app.use(cors());
@@ -14,8 +16,12 @@ import companyRouter from './src/routes/company.js';
 app.use('/', companyRouter)
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Listening on Port ${port}...`);
+app.listen(port, (error) => {
+  if(error) {
+    console.log('Something went wrong...')
+  } else {
+    console.log(`Listening on Port ${port}...`);
+  }
 });
 
 export default app;
--- src/db/db.js
+++ src/db/db.js
@@ -1,14 +1,19 @@
-import pg from 'pg';
+import mysql from 'mysql';
 import dotenv from 'dotenv';
+import util from 'util';
 
-dotenv.config();
+dotenv.config()
 
-const pool = new pg.Pool(
-    {connectionString: process.env.DB_URL || process.env.DATABASE_URL}
-);
+const dbconfig = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  connectionLimit : 100,
+};
 
-pool.connect();
+const pool = mysql.createPool(dbconfig || process.env.CLEARDB_DATABASE_URL); //using pool to avoid restarting the server every time, amongst other things. Mention them in the blog post
 
+pool.query = util.promisify(pool.query)
 
 export default pool;
-
